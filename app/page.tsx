4b14c2b0b973bc3887c4ea1609ca2a51cc0380bb
Boubacar Ballo <bab9755@nyu.edu>
2024-10-05 17:57:04 -0400

    upload linear algebra resources

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,43 @@
 'use client';
 import { useState, useEffect } from 'react';
 import Image from "next/image";
-
+import { useUser } from "@clerk/nextjs";
+import { useSignUp } from '@clerk/clerk-react'; // Import Clerk's useSignUp hook
 export default function Home() {
 
   const [data, setData] = useState('');
-
-  const connectToDB = async () => {
-    const response = await fetch('/api/users', {
-      method: 'GET', // Specify the HTTP method (GET, POST, etc.)
-    });
-    const data = await response.json();
-    const message = data?.message;
-    setData(message);
-  }
+  
+ 
+  const { signUp } = useSignUp();
+  const {isLoaded, isSignedIn, user} = useUser();
 
   useEffect(() => {
-    connectToDB(); // Make sure this is called to fetch data
-}, []); // Empty dependency array to run once on compon
-
-  
-  
+    const saveToMongoDB = async () => {
+      if (isSignedIn) {
+        const response = await fetch('/api/users', { // Adjust the API endpoint as necessary
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(
+            {
+              id: user?.id,
+              email: user?.emailAddresses[0].emailAddress,
+              firstName: user?.firstName,
+              lastName: user?.lastName,
+            }
+          ),
+        });
+        if (!response.ok){
+          console.log('There was an error saving to MongoDB')
+        }
+      }
+    }
+   saveToMongoDB();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>This is your data: {data}</h1>
+      <h1>Hello, {user && user?.firstName}</h1>
     </div>
   );
 }
